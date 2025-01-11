af7d69141f591296335a210d55a459124c24415b
[Aditya <[www.aditya117@gmail.com]>
2025-01-11 11:29:10 +0530

    basicsServletJSP added

---
+++ 03_BasicServletJSP/src/main/java/com/adi/myservlets/WelcomeServlet.java
@@ -0,0 +1,42 @@
+package com.adi.myservlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public WelcomeServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.println("<html>");
+		out.println("<head><title>Welcome - Adi App</title></head>");
+		out.println("<body>");
+		out.println("<h1>Welcome to Adiking App</h1>");
+		out.printf("<b>Current data and time is : </b> %s%n" , new Date().toString());
+		out.println("</body>");
+		out.println("</html>");	
+	}
+
+}
